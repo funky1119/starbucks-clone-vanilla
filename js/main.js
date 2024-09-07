ddb7256bbc77dc81c1ecbcdddd2358a12602661b
funky1119 <funky1119@naver.com>
2024-09-07 09:00:57 +0900

    헤더 구현

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// 검색어 클릭시 확장
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+searchEl.addEventListener("click", () => {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", () => {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+searchInputEl.addEventListener("blur", () => {
+  searchEl.classList.remove("focused");
+  searchInputEl.removeAttribute("placeholder");
+});
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      // 배치 숨기기
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배치 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간)
