8bd4426f3d5775dd7fb9e5ddcde328d1b0a12d98
funky1119 <funky1119@naver.com>
2024-09-09 18:33:24 +0900

    youtube 영역 구현

--- js/main.js
+++ js/main.js
@@ -84,3 +84,21 @@ promotionToggleBtn.addEventListener("click", () => {
     promotionEl.classList.remove("hide");
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay),
+  });
+}
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
